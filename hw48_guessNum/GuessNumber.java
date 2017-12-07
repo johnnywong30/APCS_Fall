f413ff7d86a05402d736cf7e4842f464f6e88d51
jwongUlti25 <jwong25@stuy.edu>
2017-12-07 18:31:09 -0500

    finish hw49 please

--- hw48_guessNum/GuessNumber.java
+++ hw48_guessNum/GuessNumber.java
@@ -32,10 +32,11 @@ public class GuessNumber
       ==================================================*/
     public GuessNumber( int a, int b ) 
     {
-	int a = 1;
-	int b = 100;
-	int _guessCtr = 1;
-	int _target = (int) (100 * Math.random() + 1);
+	_lo = a;
+	_hi = b;
+	_guessCtr = 1;
+	//_target = 38; // follow the model
+	_target = (int) (Math.random() * _hi + _lo); // target is in range [_lo, _hi]
 	
     }
 
@@ -43,24 +44,66 @@ public class GuessNumber
     /*==================================================
       void playRec() -- Prompts a user to guess until guess is correct.
       Uses recursion.
-      pre:  
-      post: 
+      pre:  instance of Keyboard is created
+            user inputs an int, and continues until it matches the target
+      post: prints out String containing number of guesses user took
       ==================================================*/
     public void playRec() 
     {
-	/* YOUR IMPLEMENTATION HERE */
+    	Keyboard input = new Keyboard();
+    	System.out.print("Guess a number from " + _lo + " to " + _hi + ": ");
+	int guess = input.readInt();
+	if (guess == _target){
+	    System.out.println("Correct! It took " + _guessCtr + " guesses");
+	    return;
+	}
+	else if (guess > _hi || guess < _lo){ // provides safety for if user is incompentent and unable to follow directive
+	    System.out.println("You have inputted a value outside of provided range. Please try again!");
+	}
+	else if (guess > _target){
+	    _hi = guess - 1; // upper bound set to include the guessed value minus 1
+	    System.out.println("Too high...");
+	    _guessCtr++; //guess count increments by 1
+	}
+	else if (guess < _target){
+	    _lo = guess + 1; // lower bound set to include the guessed value plus 1
+	    System.out.println("Too low...");
+	    _guessCtr++; //guess count increments by 1
+	}
+        this.playRec();
+	
     }
 
 
     /*==================================================
       void playIter() -- Prompts a user to guess until guess is correct.
       Uses iteration.
-      pre:  
-      post: 
+      pre:  instance of Keyboard is created
+            user inputs an int, and continues until it matches the target
+      post: prints out String containing number of guesses user took
       ==================================================*/
     public void playIter() 
     {
-	/* YOUR IMPLEMENTATION HERE */
+	Keyboard input = new Keyboard();
+	int guess = 0;
+	while (guess != _target){
+	    System.out.print("Guess a number from " + _lo + " to " + _hi + ": ");
+	    guess = input.readInt();
+	    if (guess > _hi || guess < _lo){ // provides safety for if user is incompentent and unable to follow directive
+		System.out.println("You have inputted a value outside of provided range. Please try again!");
+	    }
+	    else if (guess > _target){
+		_hi = guess - 1; // upper bound set to include the guessed value minus 1
+		System.out.println("Too high...");
+		_guessCtr++; // guess count increments by 1
+	    }
+	    else if (guess < _target){
+		_lo = guess + 1; // lower bound set to include the guessed value plus 1
+		System.out.println("Too low...");
+		_guessCtr++; //guess count increments by 1
+	    }
+	}
+	System.out.println("Correct! It took " + _guessCtr + " guesses");
     }
 
 
@@ -68,21 +111,22 @@ public class GuessNumber
     public void play() 
     { 
 	//use one or the other below:
-	//playRec();
-	playIter();
+	//playIter(); // I figured out the iteration first since it's easier to think about for me
+	playRec();    // but I prefer thinking recursively - but this case is different since you're not literally passing a "smaller case" to the method.
+	              // Instead the instance of GuessNumber's instance variables are being changed and are used over and over, as the range gets to be a "smaller case"
+	              // with each guess the user makes
     }
 
 
     //main method to run it all
     public static void main( String[] args ) 
     {
-	/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	
 	//instantiate a new game
 	GuessNumber g = new GuessNumber(1,100);
 
 	//start the game
 	g.play();
-	~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
     }//end main
 
 }//end class
